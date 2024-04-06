4050e79ffdef6fc76d1ac2e20143f47f3dc3095a
ricleta <ricleta30052003@gmail.com>
2024-04-06 15:50:10 -0300

    Changed scalar_matrix_mult to use intel intrinsics

--- matrix_lib.c
+++ matrix_lib.c
@@ -1,76 +1,79 @@
 // Ricardo Bastos Leta Vieira 2110526
 
-// A+0 | B+0 | C+0
-// A+0 | B+1 | C+1
-// A+1 | B+2 | C+0
-// A+1 | B+3 | C+1
-// A+2 | B+4 | C+0
-// A+2 | B+5 | C+1
-// A+3 | B+6 | C+0
-// A+3 | B+7 | C+1
-// A+4 | B+0 | C+2
-// melhor metodo
-// percorrer tudo por linha
-// iterar pelos elementos de uma linha de a
-    // multiplicar pelos elementos da msm linha de b
-    // voltar ao comeco de c e multiplicar pela proxima linha de b
-
-// manter ponteiro para o inicio da linha para resetar 
-// NAO USAR INDEXACAO
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <immintrin.h>
 #include "matrix_lib.h"
 
 int matrix_matrix_mult(struct matrix *a, struct matrix *b, struct matrix *c)
 {
-    if (a->width != b->height)
-    {
-        perror("Impossivel multplicar matrizes de tamanhos imcompativeis");
-        exit(-1);
-    }
+  if (a->width != b->height)
+  {
+    fprintf(stderr, "Impossivel multplicar matrizes de tamanhos imcompativeis\n");
+    return 0;
+  }
 
-    float *iniB = b->rows;
-    float *iniC = c->rows;
-    float *iterA = a->rows;
-    float *iterB = b->rows;
-    float *iterC = c->rows;
+  // float *iniB = b->rows;
+  float *iniC = c->rows;
+  float *iterA = a->rows;
+  float *iterB = b->rows;
+  float *iterC = c->rows;
 
-    // ocorre numero de vezes = numero de linhas de A
-    for (int i = 0; i < a->height; i++)
+  for (int i = 0; i < a->height; i++, iterA += a->width)
+  {
+    iterC = iniC;
+
+    for (int j = 0; j < b->width; j++, iterC += b->width)
     {
-      // reseta b
-      iterB = iniB;
-      // ocorre numero de vezes = numero de linhas de B
-      for (int j = 0; j < b->height; j++)
+      __m256 c = _mm256_setzero_ps();
+      float *a_row = iterA;
+      float *b_col = iterB + j;
+      __m256 b_column = _mm256_setzero_ps();
+
+      for (int k = 0; k < a->width; k += 8, a_row += 8, b_col += 8 * b->width)
       {
-        // seta iterador de C para o comeco da linha atual de C
-        iterC = iniC + i * c->width;
+        __m256 a = _mm256_loadu_ps(a_row);
 
-        // itera pelos elementos na linha atual de C e D
-        for (int k = 0; k < c->width; k++)
+        for (int l = 0; l < 8; l++)
         {
-          // printf("%f += %f * %f\n", *iterC, *iterA, *iterB);
-          *iterC++ += *iterB++ * *iterA;
+          float b_val = iterB[l * b->width]; // get the l-th element in the column
+          b_col[l] = b_val;                  // store the value in the vector
         }
 
-        iterA++; // toda vez que a linha de B muda, passa para o proximo elemento de A
+        b_column = _mm256_loadu_ps(b_col);
+        
+        // printf("----------\n");
+        // printf("i: %d, j: %d, k: %d\n", i, j, k);
+        // printf("a_row: %f %f %f %f %f %f %f %f\n", a_row[0], a_row[1], a_row[2], a_row[3], a_row[4], a_row[5], a_row[6], a_row[7]);
+        // printf("b_col: %f %f %f %f %f %f %f %f\n", b_col[0], b_col[1], b_col[2], b_col[3], b_col[4], b_col[5], b_col[6], b_col[7]);
+        // printf("----------\n");
+
+        c = _mm256_fmadd_ps(a, b_column, c);
       }
+      _mm256_storeu_ps(iterC, c);
+
+      // printf("##############\n");
+      // printf("c: %f %f %f %f %f %f %f %f\n", iterC[0], iterC[1], iterC[2], iterC[3], iterC[4], iterC[5], iterC[6], iterC[7]);
+      // printf("##############\n");
     }
-  
-    return 1;
+  }
+
+  return 1;
 }
 
-// TODO
-// Atualizar para specs do trabalho
 int scalar_matrix_mult(float scalar_value, struct matrix *matrix)
 {
-    float *p = matrix->rows;
-  
-    for (long i = 0; i < matrix->height*matrix->width; i++)
-    {
-        *p++ *= scalar_value;
-    }
+  float *p = matrix->rows;
+  __m256 scalar = _mm256_set1_ps(scalar_value);
 
-    return 1;
+  for (long i = 0; i < matrix->height * matrix->width; i += 8)
+  {
+    __m256 row = _mm256_loadu_ps(p);
+    row = _mm256_mul_ps(row, scalar);
+    _mm256_storeu_ps(p, row);
+    
+    p += 8;
+  }
+
+  return 1;
 }
