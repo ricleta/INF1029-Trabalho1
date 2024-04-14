4f755790f189700e3280cd4fdec543de6a07180c
ricleta <ricleta30052003@gmail.com>
2024-04-14 14:42:10 -0300

    Updated to use thread matrix_matrix multiplication

--- matrix_lib.c
+++ matrix_lib.c
@@ -1,23 +1,5 @@
 // Ricardo Bastos Leta Vieira 2110526
 
-// A+0 | B+0 | C+0
-// A+0 | B+1 | C+1
-// A+1 | B+2 | C+0
-// A+1 | B+3 | C+1
-// A+2 | B+4 | C+0
-// A+2 | B+5 | C+1
-// A+3 | B+6 | C+0
-// A+3 | B+7 | C+1
-// A+4 | B+0 | C+2
-// melhor metodo
-// percorrer tudo por linha
-// iterar pelos elementos de uma linha de a
-    // multiplicar pelos elementos da msm linha de b
-    // voltar ao comeco de c e multiplicar pela proxima linha de b
-
-// manter ponteiro para o inicio da linha para resetar 
-// NAO USAR INDEXACAO
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <immintrin.h>
@@ -33,6 +15,73 @@ typedef struct _scalar_thread_args
     struct matrix *matrix;
 } Scalar_thread_args;
 
+typedef struct _matrix_matrix_thread_args
+{
+    int thread_id;
+    struct matrix *a;
+    struct matrix *b;
+    struct matrix *c;
+} Matrix_matrix_thread_args;
+
+void *aux_matrix_matrix_mult(void *args_void)
+{
+  Matrix_matrix_thread_args *args = (Matrix_matrix_thread_args *)args_void;
+
+  struct matrix *a = args->a;
+  struct matrix *b = args->b;
+  struct matrix *c = args->c;
+
+  int n_linhas = args->thread_id * (a->height / NUM_THREADS);
+  
+  int iniA = n_linhas;
+  int endA = iniA + (n_linhas);
+
+  float *iniB = b->rows;
+  float *iniC = c->rows;
+  float *iterA = a->rows + iniA;
+  float *iterB = b->rows;
+  float *iterC = iniC;
+
+  __m256 aux_a = _mm256_set1_ps(*iterA);
+  __m256 aux_b;
+  __m256 aux_c;
+
+  // occurs number of times = number of rows of A
+  for (int i = iniA; i < endA; i++)
+  {
+    // reset b
+    iterB = iniB;
+
+    // occurs number of times = number of rows of B
+    for (int j = 0; j < b->height; j++)
+    {
+      // set C iterator to the beginning of the current row of C
+      iterC = iniC + i * c->width;
+
+      // iterate through the elements in the current row of C and D
+      for (int k = 0; k < c->width; k += 8)
+      {
+        // load 8 elements from B and C
+        aux_b = _mm256_loadu_ps(iterB);
+        aux_c = _mm256_loadu_ps(iterC);
+
+        // multiply and add current element of A by the elements of B
+        aux_c = _mm256_fmadd_ps(aux_a, aux_b, aux_c);
+        _mm256_storeu_ps(iterC, aux_c);
+
+        iterB += 8;
+        iterC += 8;
+      }
+
+      // every time the row of B changes, move to the next element of A
+      iterA++;
+      aux_a = _mm256_set1_ps(*iterA);
+    }
+  }
+
+  pthread_exit(NULL);
+}
+
 int matrix_matrix_mult(struct matrix *a, struct matrix *b, struct matrix *c)
 {
     if (a->width != b->height)
@@ -40,56 +89,44 @@ int matrix_matrix_mult(struct matrix *a, struct matrix *b, struct matrix *c)
         fprintf(stderr, "Impossivel multplicar matrizes de tamanhos imcompativeis\n");
         exit(2);
     }
-
-    float *iniB = b->rows;
-    float *iniC = c->rows;
-    float *iterA = a->rows;
-    float *iterB = b->rows;
-    float *iterC = c->rows;
   
-    __m256 aux_a = _mm256_set1_ps(*iterA);
-    __m256 aux_b;
-    __m256 aux_c;
+    pthread_t threads[NUM_THREADS];
 
-    // ocorre numero de vezes = numero de linhas de A
-    for (int i = 0; i < a->height; i++)
+    Matrix_matrix_thread_args *args;
+
+    if (!(args = (Matrix_matrix_thread_args *)malloc(NUM_THREADS * sizeof(Matrix_matrix_thread_args))))
     {
-      // reseta b
-      iterB = iniB;
-
-      // ocorre numero de vezes = numero de linhas de B
-      for (int j = 0; j < b->height; j++)
-      {
-        // seta iterador de C para o comeco da linha atual de C
-        iterC = iniC + i * c->width;
-
-        // itera pelos elementos na linha atual de C e D
-        for (int k = 0; k < c->width; k += 8)
-        {
-          // carrega 8 elementos de B e C
-          aux_b = _mm256_loadu_ps(iterB);
-          aux_c = _mm256_loadu_ps(iterC);
-
-          // multiplica e soma elemento atual de A pelos elemtos de B 
-          aux_c = _mm256_fmadd_ps(aux_a, aux_b, aux_c);
-          _mm256_storeu_ps(iterC, aux_c);
-
-          iterB += 8;
-          iterC += 8;
-        }
-
-        // toda vez que a linha de B muda, passa para o proximo elemento de A
-        iterA++; 
-        aux_a = _mm256_set1_ps(*iterA);
-      }
+      fprintf(stderr, "Erro ao alocar memoria para thread args em matrix_matrix_mult\n");
+      exit(4);
     }
-  
+
+    // Cria as threads
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        args[i].thread_id = i;
+        args[i].a = a;
+        args[i].b = b;
+        args[i].c = c;
+
+        pthread_create(&threads[i], NULL, aux_matrix_matrix_mult, &args[i]);
+    }
+
+    // Espera todas as threads terminarem
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+      pthread_join(threads[i], NULL);
+    }
+
+    free(args);
+    
     return 1;
 }
 
 // Funcao auxiliar para multiplicacao de matriz por escalar chamada pelas threads
-void *aux_scalar_mult(Scalar_thread_args *args)
+void *aux_scalar_mult(void *args_void)
 {
+  Scalar_thread_args * args = (Scalar_thread_args *)args_void;
+
   int thread_id = args->thread_id;
   int matrix_size = args->matrix->height * args->matrix->width;
   int ini = thread_id * (matrix_size / NUM_THREADS);
@@ -120,7 +157,7 @@ int scalar_matrix_mult(float scalar_value, struct matrix *matrix)
 
     if (!(args = (Scalar_thread_args *)malloc(NUM_THREADS * sizeof(Scalar_thread_args))))
     {
-      fprintf(stderr, "Erro ao alocar memoria para thread args\n");
+      fprintf(stderr, "Erro ao alocar memoria para thread args em scalar_matrix_mult\n");
       exit(3);
     }
 
